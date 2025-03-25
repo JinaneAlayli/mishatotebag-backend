8a3e102b9cd4c8bfb3813889786452042b4e3939
Jinane <jinanealayli@gmail.com>
2025-03-25 12:45:03 +0200

    production

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,7 +79,7 @@ exports.login = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 24 * 60 * 60 * 1000
         });
 
         const { password: _, ...userData } = user.dataValues;
